160bc0cef2ddf6b3f80e9f4fcc20527b73365245
oguzkagnici <oguzkagnici2002@hotmail.com>
2025-06-08 23:53:33 +0300

    Implement batch inference for experiment 1

--- src/experiment_1/distance_helper.py
+++ src/experiment_1/distance_helper.py
@@ -21,62 +21,64 @@ class ComputeDistance(RecordActivations):
         correct_paths = [correct_paths[i] for i in np.random.choice(range(len(correct_paths)), np.min([N, len(correct_paths)]), replace=False)]
         return correct_paths
 
-    def compute_cosine_set(self, set, transform, path_save_fig, stats, distance_type):
+    def compute_cosine_set(self, set, transform, path_save_fig, stats, distance_type, batch_size=32):
         distance = {}
 
         images = [[transform(i[0]), transform(i[1])] for i in set]
         image_plt = [[conver_tensor_to_plot(i, stats['mean'], stats['std']) for i in j] for j in images]
-        save_fig_pair(path_save_fig, image_plt, n=4)
-        with torch.no_grad():
-            for (image0, image1) in tqdm(images):
+        if path_save_fig is not None:
+            save_fig_pair(path_save_fig, image_plt, n=4)
 
-                if self.net._get_name() == 'PredNet':
-                    image0 = torch.cat([image0.unsqueeze(0)] * 2, 0).unsqueeze(0)
-                    image1 = torch.cat([image1.unsqueeze(0)] * 2, 0).unsqueeze(0)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        im0_full = torch.stack([pair[0] for pair in images], dim=0)
+        im1_full = torch.stack([pair[1] for pair in images], dim=0)
+        N = im0_full.size(0)
 
+        for start in range(0, N, batch_size):
+            end = min(start + batch_size, N)
+            im0_chunk = im0_full[start:end].to(device)
+            im1_chunk = im1_full[start:end].to(device)
+
+            # Forward chunk0
+            _ = self.net(im0_chunk)
+            first_chunk_acts = {
+                name: feat.detach().view(feat.size(0), -1)
+                for name, feat in self.activation.items()
+                if any(k in name for k in self.only_save)
+            }
+
+            # Forward chunk1
+            _ = self.net(im1_chunk)
+            second_chunk_acts = {
+                name: feat.detach().view(feat.size(0), -1)
+                for name, feat in self.activation.items()
+                if any(k in name for k in self.only_save)
+            }
+
+            for name in first_chunk_acts:
+                A, B = first_chunk_acts[name], second_chunk_acts[name]
+                if distance_type == 'cossim':
+                    dist_vals = torch.nn.functional.cosine_similarity(A, B, dim=1).cpu().tolist()
                 else:
-                    image0 = image0.unsqueeze(0)  # add batch
-                    image1 = image1.unsqueeze(0)
-
-                self.net(make_cuda(image0, torch.cuda.is_available()))
-                first_image_act = {}
-                activation_image1 = deepcopy(self.activation)
-                for name, features1 in self.activation.items():
-                    if not np.any([i in name for i in self.only_save]):
-                        continue
-                    first_image_act[name] = features1.flatten()
-
-                self.net(make_cuda(image1, torch.cuda.is_available()))
-                activation_image2 = deepcopy(self.activation)
-
-                second_image_act = {}
-                for name, features2 in self.activation.items():
-                    if not np.any([i in name for i in self.only_save]):
-                        continue
-                    second_image_act[name] = features2.flatten()
-                    if name not in distance:
-                        distance[name] = []
-                    if distance_type == 'cossim':
-                        distance[name].append(torch.nn.CosineSimilarity(dim=0)(first_image_act[name], second_image_act[name]).item())
-                    if distance_type == 'euclidean':
-                        distance[name].append(torch.norm((first_image_act[name] - second_image_act[name])).item())
+                    dist_vals = torch.norm(A - B, dim=1).cpu().tolist()
+                distance.setdefault(name, []).extend(dist_vals)
 
         return distance
 
 
-    def compute_random_set(self, transform, N=5, path_save_fig=None, stats=None, draw_obj=None, distance_type=None):
+    def compute_random_set(self, transform, N=5, path_save_fig=None, stats=None, draw_obj=None, distance_type=None, batch_size=32):
         cossim_all = {}
         im_types = {}
         for i in range(N):
             [im_types.setdefault(k, []).append(v) for k, v in draw_obj.get_all_sets()[0].items()]
 
         for idx, (type, im_samples) in enumerate(im_types.items()):
-            cossim_all[type] = self.compute_cosine_set(im_samples, transform, path_save_fig + f'/{type}.png', stats, distance_type)
+            cossim_all[type] = self.compute_cosine_set(im_samples, transform, path_save_fig + f'/{type}.png', stats, distance_type, batch_size=batch_size)
 
         return cossim_all
 
 
-def compute_distance_set(config, out_path):
+def compute_distance_set(config, out_path, batch_size=32):
     config.model, norm_stats, resize = MyGrabNet().get_net(config.network_name,
                                        imagenet_pt=True if config.pretraining == 'ImageNet' else False)
     prepare_network(config.model, config, train=False)
@@ -91,7 +93,7 @@ def compute_distance_set(config, out_path):
 
     pathlib.Path(os.path.dirname(out_path)).mkdir(parents=True, exist_ok=True)
     recorder = ComputeDistance(net=config.model, use_cuda=False, only_save=['Conv2d', 'Linear'])
-    cossim = recorder.compute_random_set(transform=transform, N=config.rep, path_save_fig=os.path.dirname(out_path), stats=norm_stats, draw_obj=config.draw_obj, distance_type=config.distance_type)
+    cossim = recorder.compute_random_set(transform=transform, N=config.rep, path_save_fig=os.path.dirname(out_path), stats=norm_stats, draw_obj=config.draw_obj, distance_type=config.distance_type, batch_size=batch_size)
 
 
     print(fg.red + f'Saved in {out_path}' + rs.fg)
