55102978be994380dd80310a9f345dc4dfc47aea
Soner Kuyar <81442313+sonerkuyar@users.noreply.github.com>
2025-06-09 21:15:31 +0300

    fix: experiment_2 contiguous slice error

--- src/experiment_2/distance_helper.py
+++ src/experiment_2/distance_helper.py
@@ -77,7 +77,7 @@ class ComputeDistance(RecordActivations):
             # Forward chunk0
             _ = self.net(im0_chunk)
             first_chunk_acts = {
-                name: feat.detach().view(feat.size(0), -1)
+                name: feat.detach().reshape(feat.size(0), -1)
                 for name, feat in self.activation.items()
                 if any(k in name for k in self.only_save)
             }
@@ -85,7 +85,7 @@ class ComputeDistance(RecordActivations):
             # Forward chunk1
             _ = self.net(im1_chunk)
             second_chunk_acts = {
-                name: feat.detach().view(feat.size(0), -1)
+                name: feat.detach().reshape(feat.size(0), -1)
                 for name, feat in self.activation.items()
                 if any(k in name for k in self.only_save)
             }
